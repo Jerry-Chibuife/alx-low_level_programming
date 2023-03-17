baac2199c6951067051e4d589523ab73bbb27356
Jerry-Chibuife <chibuifejerryu2003@gmail.com>
2023-03-16 17:50:44 -0700

    C program that prints to terminal

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* This is the entry point of the program */
+// This is the entry point of the program
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
