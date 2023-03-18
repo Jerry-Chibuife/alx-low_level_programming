420320caa7590f8f1a2226e38d2ba0931783a543
Jerry-Chibuife <chibuifejerryu2003@gmail.com>
2023-03-17 19:51:17 -0700

    C program that prints all possible combinations for single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for program
+ * Description: Prints all possible combinations for single digits
+ * Return: 0 for successful program exit
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 100; x++)
+	{
+		putchar(x + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
