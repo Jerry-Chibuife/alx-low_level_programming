bf59aa3d3ff15286afe6451670aa1e532f7d2577
Jerry-Chibuife <chibuifejerryu2003@gmail.com>
2023-03-17 19:16:35 -0700

    C program that prints 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * Description: Prints numbers from 0-9
+ * Return: 0 for successful program exit
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x+'0');
+	}
+	putchar('\n');
+	return (0);
+}
