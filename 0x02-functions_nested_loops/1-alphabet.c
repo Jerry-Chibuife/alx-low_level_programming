24132c7fd17611b1b7f398c5e200ebda29ecf57e
Jerry-Chibuife <chibuifejerryu2003@gmail.com>
2023-03-22 16:46:33 -0700

    C program that contains function that prints all alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <main.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point of program
+ * print_alphabet - prints all alphabet in lowercase
+ * Return: 0 for successful program exit
+ */
+void print_alphabet(void);
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'A'; x < 'Z'; x++)
+	{
+		_putchar(tolower(x));
+	}
+	_putchar('\n');
+}
